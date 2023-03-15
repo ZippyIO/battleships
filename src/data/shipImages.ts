e97e12408958df5967d12f48bd34135c27dc8b86
Nick <delta2888@gmail.com>
2023-03-15 14:28:44 +1100

    ✨ feat: add ship images

---
+++ src/data/shipImages.ts
@@ -0,0 +1,34 @@
+import SubmarineImg1 from '../assets/ships/submarine_1.png';
+import SubmarineImg2 from '../assets/ships/submarine_2.png';
+import NuclearSubmarineImg1 from '../assets/ships/nuclear_submarine_1.png';
+import NuclearSubmarineImg2 from '../assets/ships/nuclear_submarine_2.png';
+import NuclearSubmarineImg3 from '../assets/ships/nuclear_submarine_3.png';
+import CarrierImg1 from '../assets/ships/carrier_1.png';
+import CarrierImg2 from '../assets/ships/carrier_2.png';
+import CarrierImg3 from '../assets/ships/carrier_3.png';
+import CarrierImg4 from '../assets/ships/carrier_4.png';
+import CarrierImg5 from '../assets/ships/carrier_5.png';
+import AmphibiousImg1 from '../assets/ships/amphibious_1.png';
+import AmphibiousImg2 from '../assets/ships/amphibious_2.png';
+import AmphibiousImg3 from '../assets/ships/amphibious_3.png';
+import AmphibiousImg4 from '../assets/ships/amphibious_4.png';
+import DestroyerImg1 from '../assets/ships/destroyer_1.png';
+import DestroyerImg2 from '../assets/ships/destroyer_2.png';
+import DestroyerImg3 from '../assets/ships/destroyer_3.png';
+import CorvetteImg1 from '../assets/ships/corvette_1.png';
+import CorvetteImg2 from '../assets/ships/corvette_2.png';
+import PatrolImg1 from '../assets/ships/patrol_1.png';
+import CoastGuardImg1 from '../assets/ships/coast_guard_1.png';
+
+const SHIP_IMAGES = {
+    submarine: [SubmarineImg1, SubmarineImg2],
+    nuclear_submarine: [NuclearSubmarineImg1, NuclearSubmarineImg2, NuclearSubmarineImg3],
+    carrier: [CarrierImg1, CarrierImg2, CarrierImg3, CarrierImg4, CarrierImg5],
+    amphibious: [AmphibiousImg1, AmphibiousImg2, AmphibiousImg3, AmphibiousImg4],
+    destroyer: [DestroyerImg1, DestroyerImg2, DestroyerImg3],
+    corvette: [CorvetteImg1, CorvetteImg2],
+    patrol: [PatrolImg1],
+    coast_guard: [CoastGuardImg1],
+};
+
+export default SHIP_IMAGES;
