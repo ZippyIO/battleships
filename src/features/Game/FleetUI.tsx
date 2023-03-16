d01d455a50772578b1f87d1406b79cde5ec064f0
Nick <delta2888@gmail.com>
2023-03-16 13:45:25 +1100

    ✨ feat: add styling when ship is placed

--- src/features/Game/FleetUI.tsx
+++ src/features/Game/FleetUI.tsx
@@ -1,24 +1,37 @@
 import clsx from 'clsx';
 import { ShipType } from './Ship';
 
+type Ship =
+    | 'carrier'
+    | 'amphibious'
+    | 'nuclear_submarine'
+    | 'destroyer'
+    | 'submarine'
+    | 'corvette'
+    | 'patrol'
+    | 'coast_guard';
+
 type Props = {
     currentShip: ShipType | null;
+    placedShips: { [ship in Ship]?: boolean };
     setShip: (ship: ShipType) => void;
 };
 
-const FleetUI = ({ currentShip, setShip }: Props) => {
+const FleetUI = ({ currentShip, placedShips, setShip }: Props) => {
     const buttonClassList = clsx(
-        'rounded-md bg-blue-500 p-2 font-medium text-gray-200 hover:bg-blue-600 active:bg-blue-700',
+        'rounded-md bg-blue-500 text-sm font-medium text-gray-200 hover:bg-blue-600 active:bg-blue-700',
+        'disabled:opacity-50 disabled:pointer-events-none',
     );
 
     return (
-        <div className="flex flex-col items-center justify-center rounded-lg bg-dark-200 p-4">
+        <div className="flex flex-col items-center justify-center rounded-md bg-dark-700 p-4">
             <p className="pb-4 text-xl font-semibold">{currentShip?.name}</p>
             <div className="flex gap-2">
                 <button
                     onClick={() => setShip({ name: 'carrier', length: 5, section: null })}
                     className={buttonClassList}
                     type="button"
+                    disabled={placedShips.carrier}
                 >
                     Aircraft Carrier (5)
                 </button>
@@ -26,6 +39,7 @@ const FleetUI = ({ currentShip, setShip }: Props) => {
                     onClick={() => setShip({ name: 'amphibious', length: 4, section: null })}
                     className={buttonClassList}
                     type="button"
+                    disabled={placedShips.amphibious}
                 >
                     Amphibious(4)
                 </button>
@@ -33,6 +47,7 @@ const FleetUI = ({ currentShip, setShip }: Props) => {
                     onClick={() => setShip({ name: 'nuclear_submarine', length: 3, section: null })}
                     className={buttonClassList}
                     type="button"
+                    disabled={placedShips.nuclear_submarine}
                 >
                     Nuclear Submarine (3)
                 </button>
@@ -40,6 +55,7 @@ const FleetUI = ({ currentShip, setShip }: Props) => {
                     onClick={() => setShip({ name: 'destroyer', length: 3, section: null })}
                     className={buttonClassList}
                     type="button"
+                    disabled={placedShips.destroyer}
                 >
                     Destroyer (3)
                 </button>
@@ -47,6 +63,7 @@ const FleetUI = ({ currentShip, setShip }: Props) => {
                     onClick={() => setShip({ name: 'submarine', length: 2, section: null })}
                     className={buttonClassList}
                     type="button"
+                    disabled={placedShips.submarine}
                 >
                     Submarine (2)
                 </button>
@@ -54,6 +71,7 @@ const FleetUI = ({ currentShip, setShip }: Props) => {
                     onClick={() => setShip({ name: 'corvette', length: 2, section: null })}
                     className={buttonClassList}
                     type="button"
+                    disabled={placedShips.corvette}
                 >
                     Corvette (2)
                 </button>
@@ -61,6 +79,7 @@ const FleetUI = ({ currentShip, setShip }: Props) => {
                     onClick={() => setShip({ name: 'patrol', length: 1, section: null })}
                     className={buttonClassList}
                     type="button"
+                    disabled={placedShips.patrol}
                 >
                     Patrol Boat (1)
                 </button>
@@ -68,6 +87,7 @@ const FleetUI = ({ currentShip, setShip }: Props) => {
                     onClick={() => setShip({ name: 'coast_guard', length: 1, section: null })}
                     className={buttonClassList}
                     type="button"
+                    disabled={placedShips.coast_guard}
                 >
                     Coast Guard (1)
                 </button>
