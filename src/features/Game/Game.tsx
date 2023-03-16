1a2ab36c995a0add8b1b1b5985e42cee691d2655
Nick <delta2888@gmail.com>
2023-03-16 13:42:59 +1100

    ✨ feat: add player ships object

--- src/features/Game/Game.tsx
+++ src/features/Game/Game.tsx
@@ -5,10 +5,26 @@ import { ShipType } from './Ship';
 
 type GameStatus = 'setup' | 'active' | 'over';
 type Player = 'player1' | 'player2';
+type Ship =
+    | 'carrier'
+    | 'amphibious'
+    | 'nuclear_submarine'
+    | 'destroyer'
+    | 'submarine'
+    | 'corvette'
+    | 'patrol'
+    | 'coast_guard';
+
+type PlayerShips = {
+    [player in Player]: {
+        [ship in Ship]?: boolean;
+    };
+};
 
 const Game = () => {
     const [gameStatus, setGameStatus] = useState<GameStatus>('setup');
     const [player, setPlayer] = useState<Player>('player1');
+    const [playerShips, setPlayerShips] = useState<PlayerShips>({ player1: {}, player2: {} });
     const [currentShip, setCurrentShip] = useState<ShipType>({
         name: 'patrol',
         length: 1,
@@ -27,59 +43,66 @@ const Game = () => {
         }
     };
 
+    const updatePlayerShips = (ship: Ship, playerId: Player) => {
+        if (playerId === 'player1') {
+            setPlayerShips((prevPlayerShips) => ({
+                ...prevPlayerShips,
+                player1: {
+                    ...prevPlayerShips.player1,
+                    [ship]: true,
+                },
+            }));
+        }
+        if (playerId === 'player2') {
+            setPlayerShips((prevPlayerShips) => ({
+                ...prevPlayerShips,
+                player2: {
+                    ...prevPlayerShips.player2,
+                    [ship]: true,
+                },
+            }));
+        }
+    };
     return (
-        <div className="flex flex-col items-center justify-center gap-2">
-            <div className="mt-2 flex flex-col items-center rounded-lg bg-dark-200 p-4">
-                <p className="pb-4 text-2xl font-bold text-cyan-400">{gameStatus}</p>
-                <div className="flex gap-1">
-                    <button
-                        onClick={() => setGameStatus('setup')}
-                        type="button"
-                        className="rounded-md bg-blue-500 p-2 font-medium text-gray-200 hover:bg-blue-600 active:bg-blue-700"
-                    >
-                        Setup
-                    </button>
-                    <button
-                        onClick={() => setGameStatus('active')}
-                        type="button"
-                        className="rounded-md bg-blue-500 p-2 font-medium text-gray-200 hover:bg-blue-600 active:bg-blue-700"
-                    >
-                        Active
-                    </button>
-                    <button
-                        onClick={() => setGameStatus('over')}
-                        type="button"
-                        className="rounded-md bg-blue-500 p-2 font-medium text-gray-200 hover:bg-blue-600 active:bg-blue-700"
-                    >
-                        Over
-                    </button>
-                </div>
-                {player === 'player1' && <p>Player 1 turn</p>}
-                {player === 'player2' && <p>Player 2 turn</p>}
-            </div>
-            <div className="flex gap-4">
-                <div>
-                    <p className="text-center text-2xl font-bold">Player 1</p>
-                    <Gameboard
-                        gameStatus={gameStatus}
-                        currentShip={currentShip}
-                        player="player1"
-                        currentPlayer={player}
-                        finishTurn={setPlayerTurn}
-                    />
-                </div>
-                <div>
-                    <p className="text-center text-2xl font-bold">Player 2</p>
-                    <Gameboard
-                        gameStatus={gameStatus}
-                        currentShip={currentShip}
-                        player="player2"
-                        currentPlayer={player}
-                        finishTurn={setPlayerTurn}
-                    />
+        <div className="mx-12 mt-16 flex flex-col items-center justify-center gap-2">
+            <div className="">
+                <div className="flex gap-4">
+                    <div className="flex flex-col items-center justify-center">
+                        <p className="text-center text-2xl font-bold">Player 1</p>
+                        <Gameboard
+                            gameStatus={gameStatus}
+                            currentShip={currentShip}
+                            player="player1"
+                            currentPlayer={player}
+                            setPlayerShips={updatePlayerShips}
+                            finishTurn={setPlayerTurn}
+                        />
+                        <FleetUI
+                            currentShip={currentShip}
+                            setShip={setShip}
+                            placedShips={playerShips.player1}
+                        />
+                    </div>
+                    <div>
+                        <div className="flex flex-col items-center justify-center">
+                            <p className="text-center text-2xl font-bold">Player 2</p>
+                            <Gameboard
+                                gameStatus={gameStatus}
+                                currentShip={currentShip}
+                                player="player2"
+                                currentPlayer={player}
+                                setPlayerShips={updatePlayerShips}
+                                finishTurn={setPlayerTurn}
+                            />
+                            <FleetUI
+                                currentShip={currentShip}
+                                setShip={setShip}
+                                placedShips={playerShips.player2}
+                            />
+                        </div>
+                    </div>
                 </div>
             </div>
-            <FleetUI currentShip={currentShip} setShip={setShip} />
         </div>
     );
 };
